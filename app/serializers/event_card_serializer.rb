560c1bd8d9a7ed205cc320e9692c2a499b4fdd0b
github username <crishanks12@gmail.com>
2019-03-29 16:35:15 -0700

    update ruby version

--- app/serializers/event_card_serializer.rb
+++ app/serializers/event_card_serializer.rb
@@ -3,7 +3,4 @@ class EventCardSerializer < ActiveModel::Serializer
 
   belongs_to :reject_id, foreign_key: 'reject_id', class_name: "Resource"
   belongs_to :accept_id, foreign_key: 'accept_id', class_name: "Resource"
-
-
-
 end
